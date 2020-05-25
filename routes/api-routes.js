157f6d8492ac56b562633a7b2b10b9ffa506ab02
ssingh <sandy.boon@gmail.com>
2020-05-25 00:01:58 -0400

    added route for creating a pet and fixed some bugs

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,18 +9,44 @@ const gamePlay = require('../gamePlay/helper');
  * Route handler to retrieve a pet for a given user
  */
 apiRouter.get('/api/pet', async function (req, res) {
-  checkIfloggedIn(req, res);
-  try {
-    const pet = await db.Pet.findOne({ where: { userid: req.user.id } });
-    // what if pet is empty?
-    if (pet === null) {
-      res.sendStatus(404);
-    } else {
-      res.json({ data: pet });
+  if (isUserLoggedIn(req, res)) {
+    try {
+      const pet = await db.Pet.findOne({ where: { userid: req.user.id } });
+      // what if pet is empty?
+      if (pet === null) {
+        res.sendStatus(404);
+      } else {
+        res.json({ data: pet });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
     }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+  }
+});
+
+/**
+ * Rout handler to CREATE a new pet for a given use
+ */
+apiRouter.post('/api/pet', async function (req, res) {
+  if (isUserLoggedIn(req, res)) {
+    try {
+      // check if the user already has a pet.
+      const existingPet = await db.Pet.findOne({
+        where: { UserId: req.user.id },
+      });
+      if (existingPet !== null) {
+        res.status(409).json({ existingPet });
+      } else {
+        // add user id to pet
+        req.body.UserId = req.user.id;
+        const newPet = await db.Pet.create(req.body);
+        res.status(201).json(newPet);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error });
+    }
   }
 });
 
@@ -50,44 +76,70 @@ apiRouter.post('/api/login', passport.authenticate('local'), function (
   res.json(req.user);
 });
 
+/**
+ * Log the user out and redirect to root i.e. the login page.
+ */
 apiRouter.get('/api/logout', async function (req, res) {
-  await db.User.update({ online: false }, { where: { id: req.user.id } });
-  req.logOut();
-  res.redirect('/');
+  try {
+    if (req.user && req.user.id) {
+      await db.User.update({ online: false }, { where: { id: req.user.id } });
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    req.logOut();
+    res.redirect('/');
+  }
 });
 
 /**
  * Route handler to return all the opponets for currently signed in user
  */
 apiRouter.get('/api/getAllOpponents', async function (req, res) {
-  console.log('Getting all opponents for a user');
-  // Get users at the same level +/- 20% and then filter out offline users and the push the default opponent and return the array
-  const userPet = await db.Pet.findOne({ where: { UserId: req.user.id } });
-  const petLevelSum = gamePlay.getSumOfPetLevels(userPet);
-  const maxLevelOfOpponent = petLevelSum + petLevelSum / 5;
-  const minLevelOfOpponent = petLevelSum - petLevelSum / 5;
-  const allOpponents = await db.Pet.findAll({
-    where: {
-      totallevel: {
-        [Op.between]: [minLevelOfOpponent, maxLevelOfOpponent],
+  if (isUserLoggedIn(req, res)) {
+    console.log('Getting all opponents for a user');
+    // Get users at the same level +/- 20% and then filter out offline users and the push the default opponent and return the array
+    const userPet = await db.Pet.findOne({ where: { UserId: req.user.id } });
+    if (userPet === null) {
+      res.status(422).json({
+        error: 'No pet is assigned to this user. Battle is impposible.',
+      });
+      return;
+    }
+    const petLevelSum = gamePlay.getSumOfPetLevels(userPet);
+    const maxLevelOfOpponent = petLevelSum + petLevelSum / 5;
+    const minLevelOfOpponent = petLevelSum - petLevelSum / 5;
+    const allOpponents = await db.Pet.findAll({
+      where: {
+        totallevel: {
+          [Op.between]: [minLevelOfOpponent, maxLevelOfOpponent],
+        },
+        UserId: {
+          [Op.not]: req.user.id,
+        },
       },
-      UserId: {
-        [Op.not]: req.user.id,
-      },
-    },
-  });
+    });
 
-  // add the default opponent, just in case no one is online
-  allOpponents.push(gamePlay.getDefaultOpponent(userPet.dataValues));
+    // add the default opponent, just in case no one is online
+    allOpponents.push(gamePlay.getDefaultOpponent(userPet.dataValues));
 
-  res.json({ allOpponents: allOpponents });
+    res.json({ allOpponents: allOpponents });
+  }
 });
 
-function checkIfloggedIn(req, res) {
+/**
+ *
+ * @param {*} req HttpRequest Object
+ * @param {*} res HttpResponse Object
+ */
+function isUserLoggedIn(req, res) {
   if (!req.user) {
     // user is not signed in
     // may be should redirect to root?
     res.redirect('/');
+    return false;
+  } else {
+    return true;
   }
 }
 
